e2792afad3b0e66df130b8cac7345f1e432c770c
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2023-10-05 09:28:53 +0200

    [bleprph] implement input* commands

--- apps/bleprph/src/screamingchannels_console.c
+++ apps/bleprph/src/screamingchannels_console.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include "screamingchannels_console.h"
 #include "screamingchannels/misc.h"
+#include "screamingchannels/input.h"
 #include "console/console.h"
 
 static void screamingchannels_process_input(struct os_event *ev);
@@ -35,8 +36,19 @@ screamingchannels_process_input(struct os_event *ev)
     else if (!strcmp(line, "mode_dump")) {
         dump_sc_state();
     }
+    else if (!strcmp(line, "input_sub")) {
+        SC_INPUT_MODE = SC_INPUT_MODE_SUB;
+        // TODO: Fill input.
+        SC_INPUT_SUB_OK = 1;
+    }
+    else if (!strcmp(line, "input_gen")) {
+        SC_INPUT_MODE = SC_INPUT_MODE_GEN;
+    }
+    else if (!strcmp(line, "input_dump")) {
+        dump_sc_input();
+    }
     else {
-        console_printf("commands: mode_train mode_attack mode_dump\n");
+        console_printf("commands: mode_train mode_attack mode_dump input_sub input_gen input_dump\n");
     }
     /* Done processing line. Add the event back to the avail_queue */
     console_line_event_put(ev);
